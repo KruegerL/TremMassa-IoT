977545a15bdc6c87fa889878cd86a56c8fd8353d
KruegerL <laura_krueger@estudante.sesisenai.org.br>
2025-10-07 09:53:25 -0300

    Conexão com Wifi

--- estacoes/estacao1/estacao1.ino
+++ estacoes/estacao1/estacao1.ino
@@ -1,17 +1,37 @@
 #include <Wifi.h>
-WifiClient client;
-const String SSID = "nome da rede";
-const String PASS = "senha da rede";
+#include <PubSubClient.h>
+
+WifiClient wifi_client;
+PubSubClient mqtt(wifi_client);
+
+const String SSID = "FIESC_IOT_EDU";
+const String PASS = "8120gv08";
+
+const String brokerURL = "test.mosquitto.org";
+const int brokerPort = 1883;
+
+const String brokerUser = "";
+const String brokerPass = "";
 
 void setup() {
   Serial.begin(115200);
-  client.begin(nome, senha);
+  client.begin(SSID, PASS);
   Serial.println("Conectado no Wifi");
   while(cliente.status() != WL_CONNECTED){
     Serial.print(".");
     delay(200);
   }
-  // put your setup code here, to run once:
+  Serial.println("Conectado com sucesso!");
+  mqtt.setServer(brokerURL.c_str(),brokerPort);
+  String clientID = "estacao1-";
+  clientID += String(random(0xffff),HEX);
+  Serial.println("Conectando ao Broker");
+  while(mqtt.connect(clientID.c_str()) == 0){
+    Serial.print(".");
+    delay(200);
+  }
+  Serial.println("\nConectado ao broker!");
+
 
 }
 
