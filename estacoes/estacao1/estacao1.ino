8f7cf5d30cb6f879eef6ebcad694988bf3e95ba3
KruegerL <laura_krueger@estudante.sesisenai.org.br>
2025-10-07 08:44:44 -0300

    add estacao1.ino

--- estacoes/estacao1/estacao1.ino
+++ estacoes/estacao1/estacao1.ino
@@ -1,4 +1,16 @@
+#include <Wifi.h>
+WifiClient client;
+const String SSID = "nome da rede";
+const String PASS = "senha da rede";
+
 void setup() {
+  Serial.begin(115200);
+  client.begin(nome, senha);
+  Serial.println("Conectado no Wifi");
+  while(cliente.status() != WL_CONNECTED){
+    Serial.print(".");
+    delay(200);
+  }
   // put your setup code here, to run once:
 
 }
